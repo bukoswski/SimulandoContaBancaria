eb150902a63f9948be44235c2808c27ed7eeaac6
Pedro Mendes <154463165+bukoswski@users.noreply.github.com>
2025-04-15 06:29:33 -0300

    Subindo Base

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,28 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+         int Numero;
+         double Saldo;
+         String NomeCliente;
+         //String Agencia;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Olá tudo bem? Como é o seu nome? ");
+        NomeCliente = input.nextLine();
+
+        System.out.println("Por favor digite o número da sua Agência: ");
+        String Agencia = input.nextLine();
+
+        System.out.println("Agora digite o número da sua conta: ");
+        Numero = input.nextInt();
+
+        System.out.println("Por fim digite o seu Saldo por favor: ");
+        Saldo = input.nextDouble();
+
+        System.out.println("Olá " + NomeCliente + " obrigado por criar uma conta em noso banco, sua agência é " + Agencia + ", conta "+ Numero + " e seu saldo é: " + Saldo + " já esta dísponivel para saque.");
+
     }
 }
